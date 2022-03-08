16ffed56d484a667aef39d58206c68f28b57b5b2
Arianna Allahyar <anallahyar@gmail.com>
2022-03-08 17:46:58 +0100

    Map used to send props for each album to Album and Artist components

--- code/src/App.js
+++ code/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import data from './data.json'
-import {Album} from 'components/Album.js'
+import { Album } from 'components/Album.js'
 
 console.log(data)
 
 export const App = () => {
   return (
     <div>
-      <Album 
-        coverImage={data.albums.items[0].images[0].url}
-        albumName={data.albums.items[0].name}
-      />
+      {data.albums.items.map(album => {
+        return <Album 
+          coverImage={album.images[0].url}
+          albumName={album.name}
+          artistName= {album.artists.map(artist => {
+            return artist.name
+          })}
+        />
+      })}
     </div>
-  )
-}
+  );
+};
--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Artist } from './Artist'
 
 export const Album = (props) => {
 	return (
     <div className="albumWrapper">
       <img src={props.coverImage} alt="album cover"/>
       <h2>{props.albumName}</h2>
+      <Artist 
+        name= {props.artistName}
+      />
     </div>
 	);
 };
