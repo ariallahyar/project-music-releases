508faa489300e37d2607ede40574a045e0f3964b
Arianna Allahyar <anallahyar@gmail.com>
2022-03-09 17:55:02 +0100

    Header componenet created

--- code/src/App.js
+++ code/src/App.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import data from './data.json'
+import Header from 'components/Header.js'
 import { Album } from 'components/Album.js'
 
 console.log(data)
 
 export const App = () => {
   return (
-    <div className="albumsWrapper">
-      {data.albums.items.map(album => {
-        return <Album 
-          key={album.id}
-          coverImage={album.images[0].url}
-          albumName={album.name}
-          albumUrl={album.external_urls.spotify}
-          artistName= {album.artists.map(artist => {
-            return artist.name
-          })}
-        />
-      })}
-    </div>
+    <main>
+      <Header />,
+      <div className="albumsWrapper">
+        {data.albums.items.map(album => {
+          return <Album
+            key={album.id}
+            coverImage={album.images[0].url}
+            albumName={album.name}
+            albumUrl={album.external_urls.spotify}
+            artistName={album.artists.map(artist => {
+              return artist.name
+            })}
+          />
+        })}
+      </div>
+    </main>
   );
 };
